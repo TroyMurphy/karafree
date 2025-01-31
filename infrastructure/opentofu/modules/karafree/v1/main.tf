1cb3ee150d1553fea788317745c3de9863a3671c
Troy Murphy <troy.murphy@lanternstudios.com>
2025-01-30 17:00:25 -0700

    Add webpubsub

--- infrastructure/opentofu/modules/karafree/v1/main.tf
+++ infrastructure/opentofu/modules/karafree/v1/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_service_plan" "sp" {
   sku_name            = "B1"
 }
 
-resource "azurerm_application_insights" "sndl_poc_ai" {
+resource "azurerm_application_insights" "ai" {
   name                = var.web_application_insights_name
   location            = var.location
   resource_group_name = var.resource_group_name
@@ -16,6 +16,26 @@ resource "azurerm_application_insights" "sndl_poc_ai" {
   tags                = var.tags
 }
 
+resource "azurerm_web_pubsub" "wps" {
+  name                = "tfex-webpubsub"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  sku      = "Standard_S1"
+  capacity = 1
+
+  public_network_access_enabled = false
+
+  live_trace {
+    enabled                   = true
+    messaging_logs_enabled    = true
+    connectivity_logs_enabled = false
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
 
 resource "azurerm_linux_web_app" "web" {
   name                          = var.web_app_name
@@ -42,11 +62,12 @@ resource "azurerm_linux_web_app" "web" {
   }
 
   app_settings = {
-    APPINSIGHTS_INSTRUMENTATIONKEY = azurerm_application_insights.sndl_poc_ai.instrumentation_key
-    ApplicationInsights__InstrumentationKey = azurerm_application_insights.sndl_poc_ai.instrumentation_key
-    APPINSIGHTS_INSTRUMENTATIONKEY          = azurerm_application_insights.sndl_poc_ai.instrumentation_key
-    APPLICATIONINSIGHTS_CONNECTION_STRING   = azurerm_application_insights.sndl_poc_ai.connection_string
+    APPINSIGHTS_INSTRUMENTATIONKEY          = azurerm_application_insights.ai.instrumentation_key
+    ApplicationInsights__InstrumentationKey = azurerm_application_insights.ai.instrumentation_key
+    APPLICATIONINSIGHTS_CONNECTION_STRING   = azurerm_application_insights.ai.connection_string
     TZ                                      = "America/Edmonton"
+    Websockets__ConnectionString            = azurerm_web_pubsub.wps.primary_connection_string
+    
   }
 }
 
